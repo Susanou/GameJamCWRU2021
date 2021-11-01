2fc86623ecb932bf527d9869c71b64856375f530
Fukurou <caos2804@gmail.com>
2021-11-01 00:58:08 -0400

    PopUp fixed and renders

--- GameJamAlone/Assets/Scripts/HintPopUp.cs
+++ GameJamAlone/Assets/Scripts/HintPopUp.cs
@@ -13,7 +13,8 @@ public class HintPopUp : MonoBehaviour
     {
         TMP_Text tmp_text = GetComponent<TMP_Text>();
         tmp_text.text = displayText;
-        tmp_text.DOFade( 0f, 0.7f );
+        //tmp_text.alpha = 0f;
+        tmp_text.DOFade( 1f, 0.7f );
         transform.DOMove( transform.position + Vector3.up, 0.75f );
     }
 }
