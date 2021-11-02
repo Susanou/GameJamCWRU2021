d14676461d82b145a26f139e31d425b4b7f3ccef
Fukurou <caos2804@gmail.com>
2021-11-02 14:44:20 -0400

    added scriptable objects for water puzzle

---
+++ GameJamAlone/Assets/Scripts/ScriptableObjects/FloatValue.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Float", menuName = "Values/Float", order = 1)]
+public class FloatValue : ScriptableObject, ISerializationCallbackReceiver
+{
+
+    public float initialValue; //Value of the object (Stays the same throught the game)
+
+    [HideInInspector]
+    public float RuntimeValue; //Value used during the game
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = initialValue; //Reset value once the game stops
+    }
+
+    public void OnBeforeSerialize()
+    {
+        
+    }
+
+}
